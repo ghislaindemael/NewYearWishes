7bd6ca6e72e5a7673565f586f67fa877ef8490e1
Ghislain Demaël <demaelghislain@gmail.com>
2024-10-10 22:18:29 +0200

    Added cookie service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CookieService} from './services/cookie/cookie.service';
+import {LanguageSelectionComponent} from './components/language-selection/language-selection.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+    imports: [RouterOutlet, LanguageSelectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'LesVoeuxDeGhislain';
+
+  constructor(private cookieService: CookieService) {
+  }
+
+  ngOnInit() {
+      //Change for deduction through position
+      if(this.cookieService.getItem("lang") == null){
+          this.cookieService.setItem('lang', 'FR');
+      }
+
+  }
 }
